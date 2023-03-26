0274e2b29dec8c68b5e74bc24525a5bbf29bb458
tryka-1213 <107329072+tryka-1213@users.noreply.github.com>
2023-03-27 00:54:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /**
  * 
- * @param {number} _lvl 
+ * @param {number} lvl 
  * @returns number
  */
-function levelToXp(_lvl) {
+function levelToXp(lvl) {
     let finalXpNeeded = 0
-    for (i = 1; i < parseInt(_lvl); i++) {
+    for (i = 1; i < parseInt(lvl); i++) {
         let xpNeeded = (i)*120 + 240
         finalXpNeeded += xpNeeded
     }
@@ -14,19 +14,19 @@ function levelToXp(_lvl) {
 
 /**
  * 
- * @param {number} _lvl 
- * @param {boolean} _isPrestige 
+ * @param {number} lvl 
+ * @param {boolean} isPrestige 
  * @returns number
  */
-function levelToMessageCount(_lvl, _isPrestige = false) {
+function levelToMessageCount(lvl, isPrestige = false) {
     // xp needed
-    let xp = levelToXp(_lvl)
+    let xp = levelToXp(lvl)
 
     // multis
     let prestigeMulti = 1.2
     
     // final calculation
-    let xpPerMessage = 30 * (_isPrestige ? prestigeMulti : 1)
+    let xpPerMessage = 30 * (isPrestige ? prestigeMulti : 1)
     let messagesRequiredForLevel = Math.round(xp / xpPerMessage)
 
     return messagesRequiredForLevel
@@ -34,10 +34,10 @@ function levelToMessageCount(_lvl, _isPrestige = false) {
 
 /**
  * 
- * @param {number} _xp 
+ * @param {number} xp 
  * @returns number
  */
-function xpToLevel(_xp) {
+function xpToLevel(xp) {
     let all = []
     let allLevelXp = 0
     
@@ -49,7 +49,7 @@ function xpToLevel(_xp) {
     }
     
     // refer to array and get the closest match
-    let goal = _xp
+    let goal = xp
     var closest = all.reduce(function(prev, curr) {
         let result = (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev)
         return result
@@ -65,19 +65,61 @@ function xpToLevel(_xp) {
 
 /**
  * 
- * @param {number} _messages 
- * @param {boolean} _isPrestige 
+ * @param {number} messages 
+ * @param {boolean} isPrestige 
  * @returns number
  */
-function messageCountToLevel(_messages, _isPrestige = false) {
+function messageCountToLevel(messages = 0, isPrestige = false) {
     // multis
     let prestigeMulti = 1.2
 
     // convert messages to raw xp
-    let xp = _messages * (30 * (_isPrestige ? prestigeMulti : 1))
+    let xp = messages * (30 * (isPrestige ? prestigeMulti : 1))
     let level = xpToLevel(xp)
 
     return level
 }
 
+
+function messageCountToXp(messages = 0, isPrestige = false) {
+    // multis
+    let prestigeMulti = 1.2
+
+    // convert messages to raw xp
+    let xp = messages * (30 * (isPrestige ? prestigeMulti : 1))
+    
+    return xp
+}
+
 new DiscordUser({xp: 1111, messages: 10000})
+
+
+
+///////////// misc ////////////////
+
+/**
+ * 
+ * @param {string} input 
+ * @param {boolean} newMembersOnly 
+ */
+function extract(input, newMembersOnly) {
+    let reg_hep = newMembersOnly ? /\((?<id>\d{16,20})\) joined  :new:/gmi : /\((?<id>\d{16,20})\) joined  /gmi
+    let reg_zep = newMembersOnly ? /:new: <@!(?<id>\d{16,20})> /gmi : /<@!(?<id>\d{16,20})> /gmi
+
+    let matches_hep = input.matchAll(reg_hep)
+    let matches_zep = input.matchAll(reg_zep)
+
+    let ids = []
+
+    for (const match of matches_hep) {
+        console.log(match.groups.id)
+        ids.push(match.groups.id)
+    }
+
+    for (const match of matches_zep) {
+        console.log(match.groups.id)
+        ids.push(match.groups.id)
+    }
+
+    return ids
+}
